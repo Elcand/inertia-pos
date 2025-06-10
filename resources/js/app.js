08fc3d095967b9afb88e0ea8217a63bf1d0163db
Elcand <tjandraraharja99@gmail.com>
2025-06-10 14:02:57 +0700

    make permission in inertia

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,7 +8,25 @@ createInertiaApp({
     },
     setup({ el, App, props, plugin }) {
         createApp({ render: () => h(App, props) })
+            .mixin({
+                methods: {
+                    hasAnyPermission: function (permissions) {
+                        let allPermissions = this.$page.props.auth.permissionsl;
+                        let hasPermission = false;
+                        permissions.forEach(function (item) {
+                            if (allPermissions[item]) hasPermission = true;
+                        });
+                        return hasPermission;   
+                    },
+                },
+            })
             .use(plugin)
             .mount(el);
+    },
+    progress: {
+        delay: 250,
+        color: "#29d",
+        includeCSS: true,
+        showSpinner: false,
     },
 });
